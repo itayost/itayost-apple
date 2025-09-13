1a1798e75dc9f86d03c9a49f324484bf4af1f9ce
Itay Ostraich <itayost1@gmail.com>
2025-09-13 05:53:57 +0300

    ok

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,8 +1,7 @@
 import { Metadata } from 'next'
-import ServicesClient from './ServicesClient'
+import ServicesPage from './ServicesPage'
 import { seoConfig } from '@/config/seo'
 
-// Generate metadata for this page
 export const metadata: Metadata = {
   title: seoConfig.pages.services.title,
   description: seoConfig.pages.services.description,
@@ -18,41 +17,6 @@ export const metadata: Metadata = {
   },
 }
 
-// Structured data for services page
-export default function ServicesPage() {
-  const serviceStructuredData = {
-    '@context': 'https://schema.org',
-    '@type': 'Service',
-    name: 'שירותי פיתוח תוכנה',
-    provider: {
-      '@type': 'Organization',
-      name: 'ITAYOST',
-      url: 'https://www.itayost.com',
-    },
-    serviceType: [
-      'פיתוח אתרים',
-      'פיתוח אפליקציות',
-      'עיצוב UI/UX',
-      'פיתוח API',
-      'אבטחת מידע',
-      'ייעוץ טכנולוגי',
-    ],
-    areaServed: {
-      '@type': 'Country',
-      name: 'Israel',
-    },
-    availableLanguage: ['Hebrew', 'English'],
-  }
-
-  return (
-    <>
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify(serviceStructuredData),
-        }}
-      />
-      <ServicesClient />
-    </>
-  )
+export default function Page() {
+  return <ServicesPage />
 }
